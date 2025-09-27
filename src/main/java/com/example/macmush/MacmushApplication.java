e5d2830d3b1e8c95aebe3dd0680876c056ddd700
macmush <cheetah@cheetahui-MacBookAir.local>
2025-09-27 03:23:27 -0700

    mac mush start

---
+++ src/main/java/com/example/macmush/MacmushApplication.java
@@ -0,0 +1,13 @@
+package com.example.macmush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacmushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacmushApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/macmush/MacmushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.macmush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacmushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
