bddfe564c9a109f752aa2ec422b34ce9c7a399cd
macmush <cheetah@cheetahui-MacBookAir.local>
2025-11-09 17:04:11 +0900

    불필요 주석 제거

--- src/main/java/com/example/macmush/advice/GlobalExceptionAdvice.java
+++ src/main/java/com/example/macmush/advice/GlobalExceptionAdvice.java
@@ -9,9 +9,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionAdvice {
 
-  /**
-   * 500 - Internal Server Error (서버 내부 오류) 위에서 처리하지 못한 모든 예외를 처리
-   */
   @ExceptionHandler(Exception.class)
   public ResponseEntity<ResponseDto> handleException(Exception e) {
     ResponseDto response = new ResponseDto(e.getMessage());
